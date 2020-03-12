77699de6b21a980e9b8dd99d785c8d6bf0870258
krumilurfigur <gabrieloberg3@gmail.com>
2020-03-12 11:01:11 +0100

    lektionen 2020-03-12

--- src/gui.java
+++ src/gui.java
@@ -2,4 +2,13 @@ import javax.swing.*;
 
 public class gui {
     private JPanel panel;
+    private JTextArea textArea1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("gui");
+        frame.setContentPane(new gui().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
